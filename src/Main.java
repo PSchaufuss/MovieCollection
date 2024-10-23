0337be4ed7c751405bf0aaba7d7e3dad58a27390
PSchaufuss <pajo0001@stud.kea.dk>
2024-10-23 16:32:01 +0200

    Assignment part 4
    1) Implemented new UserInterface class.
    
    2) Moved user dialogue from 'main' class to 'userInterface' class.

--- src/Main.java
+++ src/Main.java
@@ -7,50 +7,10 @@ public class Main
         Scanner scanner = new Scanner(System.in);
         MovieCollection collection = new MovieCollection();
         Controller controller = new Controller(collection, scanner);
+        UserInterface ui = new UserInterface(scanner, controller);
 
-
-        boolean running = true;
-
-        while (running)
-        {
-            System.out.println("The Totally-Awesome Movie Collection!\n");
-            System.out.println("1. Add a new movie");
-            System.out.println("2. Show all movies");
-            System.out.println("3. Search for a movie by title");
-            System.out.println("4. Search for movies by part of the title");
-            System.out.println("5. Exit");
-            System.out.print("Choose an option: ");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice)
-            {
-                case 1:
-                    controller.addMovie();
-                    break;
-
-                case 2:
-                    controller.displayAllMovies();
-                    break;
-
-                case 3:
-                    controller.searchMovieByTitle();
-                    break;
-
-                case 4:
-                    controller.searchMovieByPartOfTitle();
-
-                case 5:
-                    running = false;
-                    System.out.println("Goodbye!");
-                    break;
-
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-
+        ui.startProgram();
         scanner.close();
+
     }
 }
---
+++ src/UserInterface.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class UserInterface
+{
+    private Scanner scanner;
+    private Controller controller;
+
+    public UserInterface(Scanner scanner, Controller controller)
+    {
+        this.scanner = scanner;
+        this.controller = controller;
+    }
+
+    public void startProgram() {
+        boolean running = true;
+
+        while (running)
+        {
+            System.out.println("The Totally-Awesome Movie Collection!\n");
+            System.out.println("1. Add a new movie");
+            System.out.println("2. Show all movies");
+            System.out.println("3. Search for a movie by title");
+            System.out.println("4. Search for movies by part of the title");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    controller.addMovie();
+                    break;
+
+                case 2:
+                    controller.displayAllMovies();
+                    break;
+
+                case 3:
+                    controller.searchMovieByTitle();
+                    break;
+
+                case 4:
+                    controller.searchMovieByPartOfTitle();
+
+                case 5:
+                    running = false;
+                    System.out.println("Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
+        scanner.close();
+
+    }
+}
